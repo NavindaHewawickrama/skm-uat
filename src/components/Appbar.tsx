a9a312ba32e78bf412cb7d8741d7e709c21c44df
NavindaHewawickrama <hewawickraman@gmail.com>
2025-04-12 15:19:08 +0530

    notification bar

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -106,7 +106,10 @@ const AppBar: React.FC<AppBarProps> = ({ toggleSideNav }) => {
                 d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
               />
             </svg>
-            <span className="absolute top-0 right-0 bg-green-500 rounded-full w-2 h-2"></span>
+            {/* Notification Count Badge */}
+            <span className="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 bg-green-500 text-white text-xs font-bold rounded-sm w-4 h-4 flex items-center justify-center">
+              3
+            </span>
           </div>
 
           {/* Alert Dropdown Menu */}
