3963e065a8270ce94e3d4ad03ba5c4833bb401e5
NavindaHewawickrama <hewawickraman@gmail.com>
2025-05-27 14:40:46 +0530

    login code

--- src/app/api/AuthApi.tsx
+++ src/app/api/AuthApi.tsx
@@ -1,3 +1,22 @@
 import axios from 'axios';
-import baseUrl from '@/app/config';
 
+//Login API
+export const Login = async (details: {
+    usernameOrEmail: any;
+    password: any;
+    rememberme: boolean;
+}) => {
+    try {
+        const response = await axios.post(`http://173.212.233.90:8090/api/User/login`, {
+            usernameOrEmail: details.usernameOrEmail,
+            password: details.password,
+            rememberme: details.rememberme
+
+        }, {
+            validateStatus: (status) => status < 500
+        });
+        return response;
+    } catch (error) {
+        throw error;
+    }
+}
