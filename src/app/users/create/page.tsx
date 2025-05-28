ae8a34383501ce1d5e2cdcbd4ef2a01e9e71a31a
PraveenBimsara <praveenbimsara99@gmail.com>
2025-05-28 16:43:44 +0530

    changes

--- src/app/users/create/page.tsx
+++ src/app/users/create/page.tsx
@@ -285,16 +285,6 @@ const CreateUserPage: React.FC = () => {
     setShowUsersList(!showUsersList);
   };
 
-  if (loading) {
-    return (
-      <div className="h-screen w-screen bg-gray-100 flex items-center justify-center">
-        <div className="text-center">
-          <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-900 mx-auto"></div>
-          <p className="mt-4 text-gray-600">Loading user creation details...</p>
-        </div>
-      </div>
-    );
-  }
 
   return (
     <div className="h-screen w-screen bg-gray-100 flex flex-col">
