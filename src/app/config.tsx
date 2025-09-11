9333612ea3291fd3e49b40ed4a47519d7c3acf33
NavindaVictory <navinda@victoryinformation.biz>
2025-09-12 03:06:15 +0530

    1.0.6 image API working

--- src/app/config.tsx
+++ src/app/config.tsx
@@ -1,4 +1,4 @@
 export default {
-    apiBaseUrl: 'https://skmsalesappapi.h2so4.lk'
-   // apiBaseUrl: 'https://skmsalesapptestapi.h2so4.lk',
+    //apiBaseUrl: 'https://skmsalesappapi.h2so4.lk'
+   apiBaseUrl: 'https://skmsalesapptestapi.h2so4.lk',
 }
